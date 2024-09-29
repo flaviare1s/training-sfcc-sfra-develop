12ee7d9b4abdab58eb7faeb8b5cf102aacbfaf38
flaviare1s <flaviarster@gmail.com>
2024-09-29 12:19:46 -0300

    Update Product.js

--- cartridges/app_storefront_custom/cartridge/controllers/Product.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Product.js
@@ -1,38 +1,7 @@
 'use strict';
 
 var server = require('server');
-var cache = require('*/cartridge/scripts/middleware/cache');
-var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
-var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
-var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
 
 server.extend(module.superModule);
-
-server.get(
-    'Show',
-    cache.applyPromotionSensitiveCache,
-    consentTracking.consent,
-    function (req, res, next) {
-        var showProductPageHelperResult = productHelper.showProductPage(
-            req.querystring,
-            req.pageMetaData
-        );
-
-        if (!showProductPageHelperResult.product.online) {
-            res.setStatusCode(404);
-            res.render('error/notFound');
-        } else {
-            res.render(showProductPageHelperResult.template, {
-                product: showProductPageHelperResult.product,
-                addToCartUrl: showProductPageHelperResult.addToCartUrl,
-                resources: showProductPageHelperResult.resources,
-                breadcrumbs: showProductPageHelperResult.breadcrumbs,
-                customData:
-                    'Adicione mais informações ou variáveis personalizadas aqui'
-            });
-        }
-        next();
-    }
-);
 
 module.exports = server.exports();
