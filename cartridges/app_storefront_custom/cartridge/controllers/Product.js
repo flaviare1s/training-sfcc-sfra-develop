c5db9d22a9eec8df4b32160a46f0e405bafccbea
flaviare1s <flaviarster@gmail.com>
2024-09-29 13:09:09 -0300

    Create suggestedProducts.isml

--- cartridges/app_storefront_custom/cartridge/controllers/Product.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Product.js
@@ -5,31 +5,40 @@ server.extend(module.superModule);
 
 var ProductMgr = require('dw/catalog/ProductMgr');
 var ProductSearchModel = require('dw/catalog/ProductSearchModel');
+var CatalogMgr = require('dw/catalog/CatalogMgr');
+var ProductSearch = require('*/cartridge/models/search/productSearch');
 
 // Append to the 'Show' route of the Product-Show controller
 server.append('Show', function (req, res, next) {
+    // Obter o ID do produto a partir da resposta
     var productId = res.getViewData().product.id;
     var product = ProductMgr.getProduct(productId);
     var suggestedProducts = [];
 
-    // Check if the product is categorized
+
     if (product.isCategorized()) {
+
         var apiProductSearch = new ProductSearchModel();
         apiProductSearch.setCategoryID(product.getPrimaryCategory().ID);
         apiProductSearch.search();
 
-        var productSearch = apiProductSearch.getProductSearchHits();
-        var index = 0;
-        while (productSearch.hasNext() && index < 4) {
-            var suggestedProductHit = productSearch.next();
-            var suggestedProductId = suggestedProductHit.getProduct().ID;
+
+        var productSearch = new ProductSearch(
+            apiProductSearch,
+            req.querystring,
+            req.querystring.srule,
+            CatalogMgr.getSortingOptions(),
+            CatalogMgr.getSiteCatalog().getRoot()
+        );
+
+
+        for (var index = 0; index < 4; index++) {
+            var suggestedProductId = productSearch.productIds[index].productID;
             var suggestedProduct = ProductMgr.getProduct(suggestedProductId);
             suggestedProducts.push(suggestedProduct);
-            index++;
         }
     }
 
-    // Pass the suggested products to the view
     res.setViewData({
         suggestedProducts: suggestedProducts
     });
