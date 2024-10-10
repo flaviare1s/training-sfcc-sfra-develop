7732faea5162a145616c1794f4c96c7922b90697
flaviare1s <flaviarster@gmail.com>
2024-10-10 20:57:53 -0300

    Update files

--- cartridges/app_storefront_custom/cartridge/controllers/Search.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Search.js
@@ -1,47 +1,23 @@
 'use strict';
 
 var server = require('server');
+var PageMgr = require('dw/experience/PageMgr');
+var CatalogMgr = require('dw/catalog/CatalogMgr');
+
 server.extend(module.superModule);
 
 server.append('Show', function (req, res, next) {
-    var PageMgr = require('dw/experience/PageMgr');
-    var CatalogMgr = require('dw/catalog/CatalogMgr');
-
-    var catId = req.querystring.cgid || null;
-
-    if (!catId) {
-        res.setStatusCode(400);
-        res.json({
-            success: false,
-            message: 'ID not provided.'
-        });
-        return next();
-    }
-
+    var catId = req.querystring.catId || ''; // Certifique-se de que catId está definido
     var category = CatalogMgr.getCategory(catId);
-
-    if (!category) {
-        res.setStatusCode(404);
-        res.json({
-            success: false,
-            message: 'Category not found.'
-        });
-        return next();
-    }
-
     var pageDesignerID =
         category && 'pageDesignerPageID' in category.custom
             ? category.custom.pageDesignerPageID
             : null;
-
     var pageDesigner = pageDesignerID ? PageMgr.getPage(pageDesignerID) : null;
 
-
     if (pageDesigner && pageDesigner.isVisible()) {
-        res.print(PageMgr.renderPage(pageDesigner.ID, ''));
-    } else {
-
-        res.render('search/noPageDesigner');
+        res.writer.println(PageMgr.renderPage(pageDesigner.ID, ''));
+        return;
     }
 
     next();
