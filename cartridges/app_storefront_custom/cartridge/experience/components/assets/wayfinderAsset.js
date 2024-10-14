041192216aef323cf2a1af528e21c6dc4f5ab8ae
flaviare1s <flaviarster@gmail.com>
2024-10-14 19:33:34 -0300

    fix: correct code in multiple files

--- cartridges/app_storefront_custom/cartridge/experience/components/assets/wayfinderAsset.js
+++ cartridges/app_storefront_custom/cartridge/experience/components/assets/wayfinderAsset.js
@@ -3,21 +3,47 @@
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
 var URLUtils = require('dw/web/URLUtils');
-var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
 
-module.exports.render = function (context, modelIn) {
-    var model = modelIn || new HashMap();
+/**
+ * Render logic for the assets.categorytile.
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
     var content = context.content;
+    var category = content.category;
+    /*
+     * If no image url was provided, clicking the category tile will lead the user back to the home page.
+     */
+    if (category) {
+        model.url = URLUtils.url('Search-Show', 'cgid', category.ID);
+    } else {
+        model.url = URLUtils.url('Home-Show');
+    }
 
-    model.image = ImageTransformation.getScaledImage(content.image);
-    model.categoryLink = URLUtils.url(
-        'Search-Show',
-        'cgid',
-        content.category.getID()
-    ).toString();
-    model.categoryName = content.category.displayName;
+    model.text_headline = content.text_headline;
+    if (content.text_subline) {
+        model.text_subline = content.text_subline;
+    }
+    model.text_alignment = content.text_alignment == 'Left' ? 'left' : 'center';
 
-    return new Template(
-        'experience/components/commerce_assets/wayfinderAsset'
-    ).render(model).text;
+    if (content.image) {
+        model.image = {
+            src: {
+                mobile: ImageTransformation.url(content.image, {
+                    device: 'mobile'
+                }),
+                desktop: ImageTransformation.url(content.image, {
+                    device: 'desktop'
+                })
+            },
+            alt: content.image.file.getAlt(),
+            focalPointX: content.image.focalPoint.x * 100 + '%',
+            focalPointY: content.image.focalPoint.y * 100 + '%'
+        };
+    }
+
+    return new Template('experience/components/assets/wayfinderAsset').render(
+        model
+    ).text;
 };
--- cartridges/app_storefront_custom/cartridge/experience/components/layouts/wayfinderLayout.js
+++ cartridges/app_storefront_custom/cartridge/experience/components/layouts/wayfinderLayout.js
@@ -1,36 +1,19 @@
 'use strict';
 
-/* global response */
-
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
-var URLUtils = require('dw/web/URLUtils');
-var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+var PageRenderHelper = require('~/cartridge/experience/utilities/PageRenderHelper.js');
 
 /**
- * Render logic for the storefront.MainBanner component
- * @param {dw.experience.ComponentScriptContext} context The Component script context object.
- * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commerce Cloud Platform.
- *
- * @returns {string} The markup to be displayed
+ * Render logic for the layouts.wayfinderLayout.
  */
-module.exports.render = function (context, modelIn) {
-    var model = modelIn || new HashMap();
-    var content = context.content;
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var component = context.component;
 
-    model.image = ImageTransformation.getScaledImage(content.image);
-    model.categoryLink = URLUtils.url(
-        'Search-Show',
-        'cgid',
-        content.categoryLink.getID()
-    ).toString();
+    // automatically register configured regions
+    model.regions = PageRenderHelper.getRegionModelRegistry(component);
 
-    // instruct 24 hours relative pagecache
-    var expires = new Date();
-    expires.setDate(expires.getDate() + 1); // this handles overflow automatically
-    response.setExpires(expires);
-
-    return new Template(
-        'experience/components/layouts/custombanner'
-    ).render(model).text;
+    return new Template('experience/components/layouts/wayfinderLayout').render(model)
+        .text;
 };
