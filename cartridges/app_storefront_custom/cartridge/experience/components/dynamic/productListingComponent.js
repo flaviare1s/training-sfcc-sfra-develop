069960e7749fedfb393362c1dd7caf0d98ff4bfb
flaviare1s <flaviarster@gmail.com>
2024-10-15 23:12:58 -0300

    Updated styles for product listing component and added attribute definitions to JSON

--- cartridges/app_storefront_custom/cartridge/experience/components/dynamic/productListingComponent.js
+++ cartridges/app_storefront_custom/cartridge/experience/components/dynamic/productListingComponent.js
@@ -6,21 +6,12 @@ var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
 var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
 
-/**
- * Render logic for the product list component
- * @param {dw.experience.ComponentScriptContext} context The Component script context object.
- * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commerce Cloud Platform.
- *
- * @returns {string} The markup to be displayed
- */
 module.exports.render = function (context, modelIn) {
     var model = modelIn || new HashMap();
-
     var component = context.component;
     model.component = component;
     model.regions = PageRenderHelper.getRegionModelRegistry(component);
     var content = context.content;
-    model.categoryId = content.category.getID();
 
     var CatalogMgr = require('dw/catalog/CatalogMgr');
     var ProductSearchModel = require('dw/catalog/ProductSearchModel');
@@ -28,23 +19,31 @@ module.exports.render = function (context, modelIn) {
     var ProductSearch = require('*/cartridge/models/search/productSearch');
 
     var apiProductSearch = new ProductSearchModel();
-    var params = { cgid: model.categoryId };
-    apiProductSearch = searchHelper.setupSearch(apiProductSearch, params);
-    var sortingRule = apiProductSearch.category.defaultSortingRule.ID;
-    apiProductSearch.search();
+    var params = {};
+    var productIds = content.productIds ? content.productIds.split(',') : [];
 
-    var productSearch = new ProductSearch(
-        apiProductSearch,
-        params,
-        sortingRule,
-        CatalogMgr.getSortingOptions(),
-        CatalogMgr.getSiteCatalog().getRoot()
-    );
-    model.productSearch = productSearch;
-    model.apiProductSearch = apiProductSearch;
-    model.maxSlots = 4;
+    if (content.category) {
+        // Prioritize category
+        params.cgid = content.category.getID();
+        apiProductSearch = searchHelper.setupSearch(apiProductSearch, params);
+        var sortingRule = apiProductSearch.category.defaultSortingRule.ID;
+        apiProductSearch.search();
 
-    // Component Regions
+        var productSearch = new ProductSearch(
+            apiProductSearch,
+            params,
+            sortingRule,
+            CatalogMgr.getSortingOptions(),
+            CatalogMgr.getSiteCatalog().getRoot()
+        );
+        model.productSearch = productSearch;
+        model.apiProductSearch = apiProductSearch;
+        model.maxSlots = 4;
+    } else if (productIds.length > 0) {
+        // Use product IDs if no category
+        model.productSearch = { productIds: productIds };
+    }
+
     var gridCol = '4';
     if (content.displayFormat && content.displayFormat.value === 'row') {
         gridCol = '12';
@@ -52,7 +51,6 @@ module.exports.render = function (context, modelIn) {
     model.gridClassName = 'region col-6 col-sm-' + gridCol;
     model.isEditMode = PageRenderHelper.isInEditMode();
 
-    // instruct 1 hour relative pagecache
     var expires = new Date();
     expires.setHours(expires.getHours() + 1);
     response.setExpires(expires);
