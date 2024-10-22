a5058ed31b13ac430615a44e4f7e029fa757d4ac
flaviare1s <flaviarster@gmail.com>
2024-10-22 12:38:10 -0300

    Update controller

--- cartridges/app_storefront_custom/cartridge/controllers/Product.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Product.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var server = require('server');
-var CatalogMgr = require('dw/catalog/CatalogMgr');
+var CategoryMgr = require('dw/catalog/CategoryMgr');
 var Logger = require('dw/system/Logger');
 
 server.extend(module.superModule);
@@ -9,22 +9,25 @@ server.extend(module.superModule);
 server.append('Show', function (req, res, next) {
     var viewData = res.getViewData();
 
-    var categoryId = req.querystring.cgid || 'defaultCategoryId';
-    var category = CatalogMgr.getCategory(categoryId);
+    // Corrigido para usar `req` em vez de `request`
+    var categoryId = req.httpParameterMap.categoryID.stringValue;
+    var category = CategoryMgr.getCategory(categoryId);
 
     if (category) {
         Logger.info('Category ID: ' + categoryId);
-        Logger.info('Category Name: ' + category.name);
-        Logger.info('Custom Attributes: ' + JSON.stringify(category.custom));
-        Logger.info('Background Color: ' + category.custom.backgroundColor);
-        Logger.info('Text Color: ' + category.custom.textColor);
-        Logger.info('Text Attribute: ' + category.custom.textAttribute);
+        Logger.info(
+            'Background Color: ' + category.custom.backgroundColor || 'not set'
+        );
+        Logger.info('Text Color: ' + category.custom.textColor || 'not set');
+        Logger.info(
+            'Text Attribute: ' + category.custom.textAttribute || 'not set'
+        );
 
         viewData.category = {
-            categoryId: categoryId,
-            backgroundColor: category.custom.backgroundColor || 'defaultColor',
-            textColor: category.custom.textColor || 'defaultTextColor',
-            textAttribute: category.custom.textAttribute || 'defaultText'
+            categoryId: category.ID,
+            backgroundColor: category.custom.backgroundColor || '',
+            textColor: category.custom.textColor || '',
+            textAttribute: category.custom.textAttribute || ''
         };
     } else {
         Logger.warn('Category not found for ID: ' + categoryId);
